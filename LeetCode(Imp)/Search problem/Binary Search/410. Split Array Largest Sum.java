fe0ca7b198da7a347fc41d185ccabf73fc85b2a0
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-11 23:14:57 +0530

    Binary Search

---
+++ LeetCode(Imp)/Search problem/Binary Search/410. Split Array Largest Sum.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public int splitArray(int[] nums, int k) {
+
+        int low = 0;
+        int high = 0;
+
+        for (int num : nums) {
+            low = Math.max(low, num);
+            high += num;
+        }
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canSplit(nums, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    public boolean canSplit(int[] nums, int k, int maxSum) {
+
+        int parts = 1;
+        int currentSum = 0;
+
+        for (int num : nums) {
+
+            if (currentSum + num > maxSum) {
+                parts++;
+                currentSum = num;
+            } else {
+                currentSum += num;
+            }
+        }
+
+        return parts <= k;
+    }
+}
