7e6c03528e0c2a2b113fb431574c66e1e4e3e7fd
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-12 12:56:55 +0530

    Binary Search

---
+++ LeetCode(Imp)/Search problem/Binary Search/1482. Minimum Number of Days to Make m Bouquets.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+
+        long required = (long) m * k;
+
+        if (required > bloomDay.length) {
+            return -1;
+        }
+
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+
+        for (int day : bloomDay) {
+            low = Math.min(low, day);
+            high = Math.max(high, day);
+        }
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (canMake(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    public boolean canMake(int[] bloomDay, int m, int k, int day) {
+
+        int flowers = 0;
+        int bouquets = 0;
+
+        for (int bloom : bloomDay) {
+
+            if (bloom <= day) {
+                flowers++;
+
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+
+            } else {
+                flowers = 0;
+            }
+        }
+
+        return bouquets >= m;
+    }
+}
